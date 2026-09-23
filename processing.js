fdba59f898c1eebb5e16fabe6955cb30663089af
ola-muaddel0 <306620868+ola-muaddel0@users.noreply.github.com>
2026-09-23 20:11:11 +0300

    Replace the number of the rows in for loop number instead num*num and set class to the square

--- processing.js
+++ processing.js
@@ -1,14 +1,14 @@
 const sketchPad = document.querySelector('.grid');
 function drawingGrid(number) {
-    for (let i = 1; i <= number * number; i++) {
-        const squareDimension = 1000 / number + "px";
-        console.log(squareDimension);
+    for (let i = 1; i <= number; i++) {
+        const squareDimension = (1000 / number) - 4 + "px";
         const row = document.createElement('div');
         row.setAttribute('class', 'row');
         for (let j = 1; j <= number; j++) {
             const square = document.createElement('div');
             square.style.width = squareDimension;
             square.style.height = squareDimension;
+            square.setAttribute('class', 'square')
             row.appendChild(square);
         }
         sketchPad.appendChild(row);
