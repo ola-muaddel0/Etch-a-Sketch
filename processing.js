ab111630abfafb5a565ef9419773acb5af1350ca
ola-muaddel0 <306620868+ola-muaddel0@users.noreply.github.com>
2026-09-23 19:59:42 +0300

    Add function to create a grid from n row that contains n column making n*n grid

--- processing.js
+++ processing.js
@@ -1,1 +1,17 @@
 const sketchPad = document.querySelector('.grid');
+function drawingGrid(number) {
+    for (let i = 1; i <= number * number; i++) {
+        const squareDimension = 1000 / number + "px";
+        console.log(squareDimension);
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        for (let j = 1; j <= number; j++) {
+            const square = document.createElement('div');
+            square.style.width = squareDimension;
+            square.style.height = squareDimension;
+            row.appendChild(square);
+        }
+        sketchPad.appendChild(row);
+    }
+}
+
