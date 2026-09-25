bc32692c7e9be9df22c820a2f3984f79ab8814ba
ola-muaddel0 <306620868+ola-muaddel0@users.noreply.github.com>
2026-09-25 16:51:14 +0300

    Create button to select the dimensuin of the grid and add it to the page

--- processing.js
+++ processing.js
@@ -1,10 +1,21 @@
 const sketchPad = document.querySelector('.grid');
+
+const selectNumBtn = document.createElement('button');
+selectNumBtn.textContent= 'Select A Number';
+selectNumBtn.setAttribute('class', 'select-number');
+
+document.body.insertBefore(selectNumBtn, sketchPad);
+
 function drawingGrid(number) {
     for (let i = 1; i <= number; i++) {
-        const squareDimension = (960 / number) + "px";
+    
+        const squareDimension = (450 / number) + "px";
+
         const row = document.createElement('div');
         row.setAttribute('class', 'row');
+
         for (let j = 1; j <= number; j++) {
+
             const square = document.createElement('div');
             square.style.width = squareDimension;
             square.style.height = squareDimension;
@@ -15,10 +26,10 @@ function drawingGrid(number) {
                 const b = Math.floor(Math.random() * 256);
                 event.target.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
             });
+
             row.appendChild(square);
         }
         sketchPad.appendChild(row);
     }
 }
-
 drawingGrid(16);
